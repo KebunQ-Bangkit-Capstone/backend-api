f0495fd7dab7db41f04b81e8e4b5559144726c64
vereniaes <titasari_1519622036@mhs.unj.ac.id>
2024-11-24 15:05:33 +0700

    update disease

--- src/api/diseases/disease.controller.ts
+++ src/api/diseases/disease.controller.ts
@@ -1,134 +1,145 @@
 import Elysia, { t } from "elysia";
 import { DiseaseService } from "./disease.service";
-import { DiseaseDTO, diseaseQuery, diseaseArrayResponse, updateDiseaseDTO, diseaseDTO, diseaseBody, diseaseResponse, DiseaseResponse } from "./disease.model";
+import {
+  DiseaseDTO,
+  diseaseQuery,
+  diseaseArrayResponse,
+  updateDiseaseDTO,
+  diseaseDTO,
+  diseaseBody,
+  diseaseResponse,
+  DiseaseResponse,
+} from "./disease.model";
 import { generalResponse } from "../../models/response.model";
 import { generalParams } from "../../models/params.model";
 import { BucketService } from "../../services/bucket.service";
 import { getFileExtension } from "../../utils/getFileExtension";
 
 export const diseaseController = new Elysia({
-    prefix: '/diseases',
-    tags: ['Diseases']
+  prefix: "/diseases",
+  tags: ["Diseases"],
 })
-    .decorate('diseaseService', new DiseaseService())
-    .decorate('bucketService', new BucketService())
+  .decorate("diseaseService", new DiseaseService())
+  .decorate("bucketService", new BucketService())
 
-    .post('/', async ({ diseaseService, bucketService, body }) => {
-        const { disease_id, image, ...rest } = body;
+  .post(
+    "/",
+    async ({ diseaseService, bucketService, body }) => {
+      await diseaseService.create(body);
+      return { message: "Disease created successfully." };
+    },
+    {
+      response: generalResponse,
+      body: diseaseBody,
+      detail: { summary: "Create Disease" },
+    }
+  )
 
-        const fileExtension = getFileExtension(image.name);
-        const fileId = `image-disease-${disease_id}.${fileExtension}`;
+  .get(
+    "/:id",
+    async ({ diseaseService, bucketService, params: { id } }) => {
+      const { image_id, ...rest } = await diseaseService.getOne(id);
+      const url = await bucketService.getSignedUrl(image_id);
 
-        await bucketService.upload(image, fileId);
+      return {
+        ...rest,
+        temporary_image_url: url,
+      };
+    },
+    {
+      params: generalParams,
+      response: diseaseResponse,
+      detail: { summary: "Get One Disease" },
+    }
+  )
 
-        const data: DiseaseDTO = {
-            disease_id: disease_id,
-            ...rest,
-            image_id: fileId,
-        };
+  .get(
+    "/",
+    async ({ diseaseService, bucketService, query }) => {
+      const { plant_index } = query;
+      const plantIndex = Number(plant_index);
 
-        await diseaseService.create(data);
-        return { message: 'Disease created successfully.' }
-    }, {
-        response: generalResponse,
-        body: diseaseBody,
-        detail: { summary: 'Create Disease' }
-    })
+      const diseasesResponse: DiseaseResponse[] = [];
 
-    .get('/:id', async ({ diseaseService, bucketService, params: { id } }) => {
-        const { image_id, ...rest } = await diseaseService.getOne(id);
+      const diseases = await diseaseService.getManyByPlantIndex(plantIndex);
+
+      for (let i = 0; i < diseases.length; i++) {
+        const { image_id, ...rest } = diseases[i];
         const url = await bucketService.getSignedUrl(image_id);
 
-        return {
-            ...rest,
-            temporary_image_url: url
+        const newDisease: DiseaseResponse = {
+          ...rest,
+          temporary_image_url: url,
         };
-    }, {
-        params: generalParams,
-        response: diseaseResponse,
-        detail: { summary: 'Get One Disease' }
-    })
 
-    .get('/', async ({ diseaseService, bucketService, query }) => {
-        const { plant_index } = query;
-        const plantIndex = Number(plant_index);
+        diseasesResponse.push(newDisease);
+      }
 
-        const diseasesResponse: DiseaseResponse[] = [];
+      console.log(diseases);
 
-        const diseases = await diseaseService.getManyByPlantIndex(plantIndex);
+      return {
+        diseases: diseasesResponse,
+      };
+    },
+    {
+      query: diseaseQuery,
+      response: diseaseArrayResponse,
+      detail: { summary: "Get Many Diseases by Plant Index" },
+    }
+  )
 
-        for (let i = 0; i < diseases.length; i++) {
-            const { image_id, ...rest } = diseases[i];
-            const url = await bucketService.getSignedUrl(image_id);
+  .get(
+    "/all",
+    async ({ diseaseService, bucketService }) => {
+      const diseasesResponse: DiseaseResponse[] = [];
 
-            const newDisease: DiseaseResponse = {
-                ...rest,
-                temporary_image_url: url
-            }
+      const diseases = await diseaseService.getMany();
 
-            diseasesResponse.push(newDisease);
-        }
+      for (let i = 0; i < diseases.length; i++) {
+        const { image_id, ...rest } = diseases[i];
+        const url = await bucketService.getSignedUrl(image_id);
 
-        console.log(diseases);
-
-        return {
-            diseases: diseasesResponse
+        const newDisease: DiseaseResponse = {
+          ...rest,
+          temporary_image_url: url,
         };
-    }, {
-        query: diseaseQuery,
-        response: diseaseArrayResponse,
-        detail: { summary: 'Get Many Diseases by Plant Index' }
-    })
 
-    .get('/all', async ({ diseaseService, bucketService }) => {
-        const diseasesResponse: DiseaseResponse[] = [];
+        diseasesResponse.push(newDisease);
+      }
 
-        const diseases = await diseaseService.getMany();
+      return {
+        diseases: diseasesResponse,
+      };
+    },
+    {
+      response: diseaseArrayResponse,
+      detail: { summary: "Get All Diseases" },
+    }
+  )
 
-        for (let i = 0; i < diseases.length; i++) {
-            const { image_id, ...rest } = diseases[i];
-            const url = await bucketService.getSignedUrl(image_id);
+  .patch(
+    "/:id",
+    async ({ diseaseService, params: { id }, body }) => {
+      await diseaseService.update(id, body);
+      return { message: "Disease updated successfully" };
+    },
+    {
+      params: generalParams,
+      body: updateDiseaseDTO,
+      response: generalResponse,
+      detail: { summary: "Update Disease" },
+    }
+  )
 
-            const newDisease: DiseaseResponse = {
-                ...rest,
-                temporary_image_url: url
-            }
-
-            diseasesResponse.push(newDisease);
-        }
-
-        return {
-            diseases: diseasesResponse
-        };
-    }, {
-        response: diseaseArrayResponse,
-        detail: { summary: 'Get All Diseases' }
-    })
-
-    .patch('/:id', async ({ diseaseService, bucketService, params: { id }, body }) => {
-        const { image, ...rest } = body;
-
-        if (image) {
-            const fileExtension = getFileExtension(image.name);
-            const fileId = `image-disease-${id}.${fileExtension}`;
-
-            await bucketService.upload(image, fileId);
-        }
-
-        await diseaseService.update(id, rest);
-        return { message: 'Disease updated successfully' }
-    }, {
-        params: generalParams,
-        body: updateDiseaseDTO,
-        response: generalResponse,
-        detail: { summary: 'Update Disease' }
-    })
-
-    .delete('/:id', async ({ diseaseService, params: { id } }) => {
-        await diseaseService.delete(id);
-        return { message: 'Disease deleted successfully' }
-    }, {
-        params: generalParams,
-        response: generalResponse,
-        detail: { summary: 'Delete Disease' }
-    })
+  .delete(
+    "/:id",
+    async ({ diseaseService, params: { id } }) => {
+      await diseaseService.delete(id);
+      return { message: "Disease deleted successfully" };
+    },
+    {
+      params: generalParams,
+      response: generalResponse,
+      detail: { summary: "Delete Disease" },
+    }
+  );
--- src/api/diseases/disease.model.ts
+++ src/api/diseases/disease.model.ts
@@ -1,55 +1,57 @@
 import { t } from "elysia";
 
 export const diseaseDTO = t.Object({
-    disease_id: t.String(),
-    plant_index: t.Number(),
-    disease_index: t.Number(),
-    description: t.String(),
-    treatment: t.String(),
-    analysis: t.String(),
-    article: t.String(),
-    image_id: t.String()
+  disease_id: t.String(),
+  plant_index: t.Number(),
+  disease_index: t.Number(),
+  description: t.String(),
+  treatment: t.String(),
+  analysis: t.String(),
+  article: t.String(),
+  image_id: t.String(),
 });
 
 export const updateDiseaseDTO = t.Object({
-    plant_index: t.Optional(t.Number()),
-    disease_index: t.Optional(t.Number()),
-    description: t.Optional(t.String()),
-    treatment: t.Optional(t.String()),
-    analysis: t.Optional(t.String()),
-    article: t.Optional(t.String()),
-    image: t.Optional(t.File({ type: 'image', maxSize: '1m', maxItems: 1 })),
+  plant_index: t.Optional(t.Number()),
+  disease_index: t.Optional(t.Number()),
+  description: t.Optional(t.String()),
+  treatment: t.Optional(t.String()),
+  analysis: t.Optional(t.String()),
+  article: t.Optional(t.String()),
+  image_id: t.String(),
 });
 
 export const diseaseBody = t.Object({
-    disease_id: t.String(),
-    plant_index: t.Number(),
-    disease_index: t.Number(),
-    description: t.String(),
-    treatment: t.String(),
-    analysis: t.String(),
-    article: t.String(),
-    image: t.File({ type: 'image', maxSize: '1m', maxItems: 1 }),
+  disease_id: t.String(),
+  plant_index: t.Number(),
+  disease_index: t.Number(),
+  description: t.String(),
+  treatment: t.String(),
+  analysis: t.String(),
+  article: t.String(),
+  image_id: t.String(),
 });
 
 export const diseaseResponse = t.Object({
-    disease_id: t.String(),
-    plant_index: t.Number(),
-    disease_index: t.Number(),
-    description: t.String(),
-    treatment: t.String(),
-    analysis: t.String(),
-    article: t.String(),
-    temporary_image_url: t.String()
+  disease_id: t.String(),
+  plant_index: t.Number(),
+  disease_index: t.Number(),
+  description: t.String(),
+  treatment: t.String(),
+  analysis: t.String(),
+  article: t.String(),
+  temporary_image_url: t.String(),
 });
 
 export const diseaseQuery = t.Object({
-    plant_index: t.String(),
+  plant_index: t.String(),
 });
 
-export const diseaseArrayResponse = t.Object({ diseases: t.Array(diseaseResponse) });
+export const diseaseArrayResponse = t.Object({
+  diseases: t.Array(diseaseResponse),
+});
 
 export type DiseaseDTO = typeof diseaseDTO.static;
 export type UpdateDiseaseDTO = typeof updateDiseaseDTO.static;
-export type DiseaseBody = typeof diseaseBody.static
+export type DiseaseBody = typeof diseaseBody.static;
 export type DiseaseResponse = typeof diseaseResponse.static;
