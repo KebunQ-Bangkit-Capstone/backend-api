fb510b4277e428c6d2743ab6233a37b0bb01e52b
vereniaes <titasari_1519622036@mhs.unj.ac.id>
2024-11-24 16:38:20 +0700

    adding prisma to postgresql

---
+++ prisma/migrations/20241124093517_dev/migration.sql
@@ -0,0 +1,60 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "user_id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "profile_image_id" TEXT,
+    "created_at" TEXT NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "Predictions" (
+    "prediction_id" TEXT NOT NULL,
+    "plant_index" INTEGER NOT NULL,
+    "disease_index" INTEGER NOT NULL,
+    "confidence_score" DOUBLE PRECISION NOT NULL,
+    "image_id" TEXT NOT NULL,
+    "created_at" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "Predictions_pkey" PRIMARY KEY ("prediction_id")
+);
+
+-- CreateTable
+CREATE TABLE "Discussions" (
+    "discussion_id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "image_id" TEXT,
+    "created_at" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "Discussions_pkey" PRIMARY KEY ("discussion_id")
+);
+
+-- CreateTable
+CREATE TABLE "Comments" (
+    "comment_id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "discussion_id" TEXT NOT NULL,
+
+    CONSTRAINT "Comments_pkey" PRIMARY KEY ("comment_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
+
+-- AddForeignKey
+ALTER TABLE "Predictions" ADD CONSTRAINT "Predictions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("user_id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Discussions" ADD CONSTRAINT "Discussions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("user_id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Comments" ADD CONSTRAINT "Comments_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("user_id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "Comments" ADD CONSTRAINT "Comments_discussion_id_fkey" FOREIGN KEY ("discussion_id") REFERENCES "Discussions"("discussion_id") ON DELETE CASCADE ON UPDATE NO ACTION;
--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,10 +1,22 @@
-import { PrismaClient } from '@prisma/client';
+import { resolve } from "node:path";
+import { PrismaClient } from "@prisma/client";
 import {
-    AuthTypes,
-    Connector,
-    IpAddressTypes,
-} from '@google-cloud/cloud-sql-connector';
+  AuthTypes,
+  Connector,
+  IpAddressTypes,
+} from "@google-cloud/cloud-sql-connector";
 
-export const prisma = new PrismaClient();
+const path = resolve(".s.PGSQL.5432"); // postgres-required socket filename
+const connector = new Connector();
+await connector.startLocalProxy({
+  instanceConnectionName: "",
+  ipType: IpAddressTypes.PUBLIC,
+  authType: AuthTypes.IAM,
+  listenOptions: { path },
+});
 
-// setup cloud sql + connector to prisma
+// note that the host parameter needs to point to the parent folder of
+// the socket provided in the `path` Connector option, in this example
+// that is going to be the current working directory
+const datasourceUrl = `postgresql://postgres:masfarhandouble@localhost/kebunq-sql`;
+export const prisma = new PrismaClient({ datasourceUrl });
